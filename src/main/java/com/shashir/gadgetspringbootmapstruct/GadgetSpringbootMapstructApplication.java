ee794abc8bbbd17771c39397db1d2e51bf8ee7d6
shashirl9 <shashidhar.lucky9@gmail.com>
2021-06-12 20:01:32 +0530

    sprint boot starter project commit

---
+++ src/main/java/com/shashir/gadgetspringbootmapstruct/GadgetSpringbootMapstructApplication.java
@@ -0,0 +1,13 @@
+package com.shashir.gadgetspringbootmapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetSpringbootMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetSpringbootMapstructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashir/gadgetspringbootmapstruct/GadgetSpringbootMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashir.gadgetspringbootmapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetSpringbootMapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
